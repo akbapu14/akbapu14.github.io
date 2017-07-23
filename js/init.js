81848c14f9dec47aa549133ef81cb69ae3d485dd
Akilesh Bapu <akilesh@berkeley.edu>
2017-07-23 00:32:53 -0700

    My website

--- js/init.js
+++ js/init.js
@@ -4,15 +4,15 @@ window.onload = function(){
 	//canvas init
 	var canvas = document.getElementById("canvas");
 	var ctx = canvas.getContext("2d");
-	
+
 	//canvas dimensions
 	var W = window.innerWidth;
 	var H = 140;
 	canvas.width = W;
 	canvas.height = H;
-	
+
 	//snowflake particles
-	var mp = 25; //max particles
+	var mp = 100; //max particles
 	var particles = [];
 	for(var i = 0; i < mp; i++)
 	{
@@ -23,12 +23,12 @@ window.onload = function(){
 			d: Math.random()*mp //density
 		})
 	}
-	
+
 	//Lets draw the flakes
 	function draw()
 	{
 		ctx.clearRect(0, 0, W, H);
-		
+
 		ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
 		ctx.beginPath();
 		for(var i = 0; i < mp; i++)
@@ -40,7 +40,7 @@ window.onload = function(){
 		ctx.fill();
 		update();
 	}
-	
+
 	//Function to move the snowflakes
 	//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
 	var angle = 0;
@@ -56,7 +56,7 @@ window.onload = function(){
 			//Lets make it more random by adding in the radius
 			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
 			p.x += Math.sin(angle) * 2;
-			
+
 			//Sending flakes back from the top when it exits
 			//Lets make it a bit more organic and let flakes enter from the left and right also.
 			if(p.x > W+5 || p.x < -5 || p.y > H)
@@ -82,7 +82,7 @@ window.onload = function(){
 			}
 		}
 	}
-	
+
 	//animation loop
 	setInterval(draw, 33);
 }
@@ -100,25 +100,25 @@ window.onload = function(){
 
 		var	$window = $(window),
 			$body = $('body');
-			
+
 		// Disable animations/transitions until the page has loaded.
 			$body.addClass('is-loading');
-			
+
 			$window.on('load', function() {
 				$body.removeClass('is-loading');
 			});
-			
+
 		// Forms (IE<10).
 			var $form = $('form');
 			if ($form.length > 0) {
-				
+
 				if (skel.vars.IEVersion < 10) {
 					$.fn.n33_formerize=function(){var _fakes=new Array(),_form = $(this);_form.find('input[type=text],textarea').each(function() { var e = $(this); if (e.val() == '' || e.val() == e.attr('placeholder')) { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).blur(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } }).focus(function() { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) return; if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); _form.find('input[type=password]').each(function() { var e = $(this); var x = $($('<div>').append(e.clone()).remove().html().replace(/type="password"/i, 'type="text"').replace(/type=password/i, 'type=text')); if (e.attr('id') != '') x.attr('id', e.attr('id') + '_fakeformerizefield'); if (e.attr('name') != '') x.attr('name', e.attr('name') + '_fakeformerizefield'); x.addClass('formerize-placeholder').val(x.attr('placeholder')).insertAfter(e); if (e.val() == '') e.hide(); else x.hide(); e.blur(function(event) { event.preventDefault(); var e = $(this); var x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } }); x.focus(function(event) { event.preventDefault(); var x = $(this); var e = x.parent().find('input[name=' + x.attr('name').replace('_fakeformerizefield', '') + ']'); x.hide(); e.show().focus(); }); x.keypress(function(event) { event.preventDefault(); x.val(''); }); });  _form.submit(function() { $(this).find('input[type=text],input[type=password],textarea').each(function(event) { var e = $(this); if (e.attr('name').match(/_fakeformerizefield$/)) e.attr('name', ''); if (e.val() == e.attr('placeholder')) { e.removeClass('formerize-placeholder'); e.val(''); } }); }).bind("reset", function(event) { event.preventDefault(); $(this).find('select').val($('option:first').val()); $(this).find('input,textarea').each(function() { var e = $(this); var x; e.removeClass('formerize-placeholder'); switch (this.type) { case 'submit': case 'reset': break; case 'password': e.val(e.attr('defaultValue')); x = e.parent().find('input[name=' + e.attr('name') + '_fakeformerizefield]'); if (e.val() == '') { e.hide(); x.show(); } else { e.show(); x.hide(); } break; case 'checkbox': case 'radio': e.attr('checked', e.attr('defaultValue')); break; case 'text': case 'textarea': e.val(e.attr('defaultValue')); if (e.val() == '') { e.addClass('formerize-placeholder'); e.val(e.attr('placeholder')); } break; default: e.val(e.attr('defaultValue')); break; } }); window.setTimeout(function() { for (x in _fakes) _fakes[x].trigger('formerize_sync'); }, 10); }); return _form; };
 					$form.n33_formerize();
 				}
 
 			}
-		
+
 		// CSS polyfills (IE<9).
 			if (skel.vars.IEVersion < 9)
 				$(':last-child').addClass('last-child');
@@ -130,7 +130,7 @@ window.onload = function(){
 				$('#nav a, .scrolly').scrolly(1000, x);
 
 			});
-		
+
 	});
 
 })(jQuery);
